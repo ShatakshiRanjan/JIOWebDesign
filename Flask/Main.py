2404386c34dc313dfc386b865dd4c36cd89f0787
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-07-02 17:33:02 +0530

    main connected to port

---
+++ Flask/Main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Here you would add authentication logic
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Here you would add registration logic
+        return redirect(url_for('home'))
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
