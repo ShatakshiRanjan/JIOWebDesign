3c4912fa9afce08340c7dc19b75159a010a938c6
pranitchopra <pranit.chopra06@gmail.com>
2024-07-08 11:52:14 +0530

    robot.html

--- Flask/Schema.sql
+++ Flask/Schema.sql
@@ -1,19 +1,19 @@
 -- Database 
-#CREATE DATABASE  IF NOT EXISTS `Taskify`
-#USE `Taskify`;
+CREATE DATABASE  IF NOT EXISTS `Taskify`
+USE `Taskify`;
 
 -- Drop tables if they exist (optional)
 #DROP TABLE IF EXISTS users;
 #DROP TABLE IF EXISTS tasks;
 
 -- Create users table
-#CREATE TABLE users (
-#    id INT AUTO_INCREMENT PRIMARY KEY,
-#    email VARCHAR(255) NOT NULL,
-#    first_name VARCHAR(100) NOT NULL,
-#    last_name VARCHAR(100) NOT NULL,
-#    passw VARCHAR(255) NOT NULL
-#);
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    passw VARCHAR(255) NOT NULL
+);
 
 -- Create tasks table
 #CREATE TABLE tasks (
