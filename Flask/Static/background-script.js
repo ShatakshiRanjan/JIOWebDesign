796a9e37f430a1d036849e5d767c2235a3e0b2e4
Raghav Kumar Prasad <raghavkumarprasad@gmail.com>
2024-07-18 12:38:41 +0530

    Major Changes

--- Flask/Static/background-script.js
+++ Flask/Static/background-script.js
@@ -1,32 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
     const stars = document.getElementById('stars');
-    const starsmall = 600;
-    const starmid = 100;
-    const starbig = 60;
+    
+    
+    var height = document.documentElement.scrollHeight;
+    
+    const starsmall = height/8;
+    const starmid = height/10;
+    const starbig = height/15;
   
     for (let i = 0; i < starsmall; i++) {
       let star = document.createElement('div');
       star.className = 'star-small';
-      star.style.top = `${Math.random() * 2000}px`;
+      star.style.top = `${Math.random() * height}px`;
       star.style.left = `${Math.random() * window.innerWidth}px`;
       star.style.animationDuration = `${2 + Math.random() * 8}s`;
-      star.style.animationDelay = `${Math.random() * 5}s`;
+      star.style.animationDelay = `${Math.random() * 2}s`;
       stars.appendChild(star);
     }
     for (let i=0; i< starmid; i++) {
       let star = document.createElement('div');
       star.className = 'star-mid';
-      star.style.top = `${Math.random() * 2000}px`;
+      star.style.top = `${Math.random() * height}px`;
       star.style.left = `${Math.random() * window.innerWidth}px`;
       star.style.animationDuration = `${2 + Math.random() * 8}s`;
-      star.style.animationDelay = `${Math.random() * 5}s`;
+      star.style.animationDelay = `${Math.random() * 3}s`;
       stars.appendChild(star);
     }
 
     for (let i=0; i< starbig; i++) {
       let star = document.createElement('div');
       star.className = 'star-big';
-      star.style.top = `${Math.random() * 2000}px`;
+      star.style.top = `${Math.random() * height}px`;
       star.style.left = `${Math.random() * window.innerWidth}px`;
       star.style.animationDuration = `${2 + Math.random() * 8}s`;
       star.style.animationDelay = `${Math.random() * 5}s`;
