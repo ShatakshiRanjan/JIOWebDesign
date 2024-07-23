0e2b43464a868701062f60b08d1efde9a3b1be05
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-07-23 11:46:26 +0530

    html task and port changes

--- Flask/Main.py
+++ Flask/Main.py
@@ -316,4 +316,4 @@ def calendar():
     return render_template('calendar.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000)
