29c1cd718ee6c45e3e5e43f96e20f26abef45350
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-07-23 17:24:26 +0530

    adding pretty buttons ig

--- Flask/Main.py
+++ Flask/Main.py
@@ -344,4 +344,4 @@ def calendar():
     return render_template('calendar.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000, debug=True)
